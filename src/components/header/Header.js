6d0cb09944e9a921b5d264f200f854cedfdc826f
LucasFacheli <lucasfacheli@hotmail.com>
2023-08-03 21:56:09 -0300

    add header and update colors

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -1,27 +1,30 @@
 import React from 'react';
-import './Header.css';
+import { faLinkedinIn, faGithub } from '@fortawesome/free-brands-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { Grid } from '@mui/material';
+import './Header.css';
 
 export default function Header() {
+  const openLinkedIn = () => {
+    window.open("https://www.linkedin.com/in/lucasfacheli/", "_blank", "noopener");
+  };
+
+  const openGithub = () => {
+    window.open("https://github.com/LucasFacheli?tab=repositories", "_blank", "noopener");
+  };
 
   return (
-    <Grid className='Header'>
-      <Grid className='Options'>
-        <div>Inicio</div>
-        <Grid>Proyectos</Grid>
-        <Grid>Contacto</Grid>
+    <Grid container className='Header'>
+      <Grid item className='Options' xs={6}>
+        <Grid item className='Option' xs={3}>Home</Grid>
+        <Grid item className='Option' xs={3}>Skills</Grid>
+        <Grid item className='Option' xs={3}>Proyects</Grid>
+        <Grid item className='Option' xs={3}>Contact</Grid>
       </Grid>
-      <Grid className='LanguageGrid'>
-        <Grid>
-          <p>ES</p>
-        </Grid>
-        <Grid>
-          <p>EN</p>
-        </Grid>
+      <Grid item className='IconGrid' xs={6}>
+          <FontAwesomeIcon icon={faLinkedinIn}  className='TypeIcon' onClick={openLinkedIn}/>
+          <FontAwesomeIcon icon={faGithub} className='TypeIcon' onClick={openGithub}/>
       </Grid>
-      {/*<div>Inicio</div>
-      <div>Mis Proyectos</div>
-  <div>Contacto</div>*/}
     </Grid>
   )
 }
